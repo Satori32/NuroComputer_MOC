721dc44c3aa1b2a0c3e63979e5e6401b954a6483
Satori32 <Huga8989+githubf@gmail.com>
2024-08-06 21:33:45 +0900

    プロジェクト ファイルを追加します。

---
+++ ソース.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <cstdint>
+
+enum class Stats {
+	None=0,
+	Spark,
+};
+
+struct Point
+{
+	std::intmax_t ID=0;
+};
+
+typedef std::shared_ptr<Point> SPoint;
+
+struct Data
+{
+	SPoint Other=nullptr;
+	Stats S;
+	double Velocity = 0;
+};
+
+struct Node
+{
+	SPoint P = nullptr;
+	std::vector<Data> Points;
+};
+
+struct System
+{
+	std::vector<Node> Nodes;
+};
+
+Node MakeNode() {
+	Node N;
+
+	N.P = std::make_shared<Point>();
+	N.Points.push_back({ std::make_shared<Point>(),Stats::None });
+
+	return N;
+
+}
+Node MakeNode(SPoint P,SPoint Other,Stats S) {
+	Node N;
+
+	N.P = P;
+	N.Points.push_back({ Other,S});
+
+	return N;
+
+}
+
+int main() {
+	System S;
+
+	S.Nodes.push_back(MakeNode());
+}
